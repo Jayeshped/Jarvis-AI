8d1b5e18b3a665b1e5303d6ff76651c548e78172
Jayeshped <pednekarjayesh29@gmail.com>
2024-08-13 22:22:09 +0530

    updated commit

---
+++ engine/command.py
@@ -0,0 +1,49 @@
+import pyttsx3
+import speech_recognition as sr
+import eel
+
+def speak(text):
+    engine=pyttsx3.init('sapi5')
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.setProperty('rate',174)
+    print(voices)
+    engine.say(text)
+    engine.runAndWait()
+
+
+@eel.expose
+def takeCommand():
+    
+    r=sr.Recognizer()
+    
+    with sr.Microphone() as source:
+        print('Listening...')
+        eel.DisplayMessage('Listening...')
+        r.pause_threshold=1
+        r.adjust_for_ambient_noise(source)
+        
+        audio=r.listen(source,10,8)
+        
+    try:
+        print('recognizing')
+        eel.DisplayMessage('recognizing...')
+        query=r.recognize_google(audio,language='en-in')
+        print(f"user said:{query}")
+        eel.DisplayMessage(query)
+        speak(query)
+        eel.ShowHood()
+    except Exception as e:
+        return ""
+    return query.lower()
+#text=takeCommand()
+            
+#speak(text)
+    
+
+
+
+
+
+
+
