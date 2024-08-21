46ad88bd10186ea73d7665a136295f08fd2981bb
Jayeshped <pednekarjayesh29@gmail.com>
2024-08-21 22:02:48 +0530

    whatsApp Updated Code commit

--- engine/features.py
+++ engine/features.py
@@ -1,16 +1,20 @@
 import os
+from pipes import quote
 import re
+import subprocess
 import time
 from playsound import playsound
 import eel
 import webbrowser
+
+import pyautogui
 from engine.command import speak
 from engine.config import ASSISTANT_NAME
 # Playing assistant sound function
 import pywhatkit as kit
 import sqlite3
 import struct
-from engine.helper import extract_yt_term
+from engine.helper import extract_yt_term, remove_words
 import pyaudio
 import pvporcupine
 
@@ -104,3 +108,63 @@ def hotword():
             audio_stream.close()
         if paud is not None:
             paud.terminate()
+            
+#Finding contacts in Whatsapp
+def findContact(query):
+        
+    words_to_remove = [ASSISTANT_NAME, 'make', 'a', 'to', 'phone', 'call', 'send', 'message', 'wahtsapp', 'video']
+    query = remove_words(query, words_to_remove)
+
+    try:
+        query = query.strip().lower()
+        cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+        results=cursor.fetchall()
+        print(results[0][0])
+        mobile_number_str=str(results[0][0])
+        if not mobile_number_str.startswith('+91'):
+            mobile_number_str='+91'+ mobile_number_str
+
+        return mobile_number_str,query
+    except:
+        speak('not exist in contacts')
+        return 0, 0
+
+#Whatsapp feature like sending message/video call/phone call
+
+def whatsApp(mobile_no, message, flag, name):
+
+    if flag == 'message':
+        target_tab = 12
+        jarvis_message = "message send successfully to "+name
+
+    elif flag == 'call':
+        target_tab = 7
+        message = ''
+        jarvis_message = "calling to "+name
+
+    else:
+        target_tab = 6
+        message = ''
+        jarvis_message = "staring video call with "+name
+
+    # Encode the message for URL
+    encoded_message = quote(message)
+    print(encoded_message)
+    # Construct the URL
+    whatsapp_url = f"whatsapp://send?phone={mobile_no}&text={encoded_message}"
+
+    # Construct the full command
+    full_command=f'start "" "{whatsapp_url}"'
+
+    # Open WhatsApp with the constructed URL using cmd.exe
+    subprocess.run(full_command, shell=True)
+    time.sleep(5)
+    subprocess.run(full_command, shell=True)
+    
+    pyautogui.hotkey('ctrl','f')
+
+    for i in range(1, target_tab):
+        pyautogui.hotkey('tab')
+
+    pyautogui.hotkey('enter')
+    speak(jarvis_message)
